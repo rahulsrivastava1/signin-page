a6af8886b63b1e7c4d5f2acd36f3ab5259a2078c
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2022-02-06 21:28:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Login from "./Login";
+
+const App = () => {
+  return (
+    <div className="g-signin">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+
+const clientId =
+  "647918111766-4uelo5dutlhnql6uo8nvcbra6n9dj9vu.apps.googleusercontent.com";
+
+function Login() {
+  const [showloginButton, setShowloginButton] = useState(true);
+  const [showlogoutButton, setShowlogoutButton] = useState(false);
+  const onLoginSuccess = (res) => {
+    console.log("Login Success:", res.profileObj);
+    setShowloginButton(false);
+    setShowlogoutButton(true);
+  };
+
+  const onLoginFailure = (res) => {
+    console.log("Login Failed:", res);
+  };
+
+  const onSignoutSuccess = () => {
+    alert("You have been logged out successfully");
+    console.clear();
+    setShowloginButton(true);
+    setShowlogoutButton(false);
+  };
+
+  return (
+    <div>
+      <h1>Login Page!</h1>
+      <h1>use your gmail accounts to login</h1>
+      {showloginButton ? (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign In"
+          onSuccess={onLoginSuccess}
+          onFailure={onLoginFailure}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+      ) : null}
+
+      {showlogoutButton ? (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText="Sign Out"
+          onLogoutSuccess={onSignoutSuccess}
+        ></GoogleLogout>
+      ) : null}
+    </div>
+  );
+}
+export default Login;
